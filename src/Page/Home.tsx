99d6c5705ff216ac66f54e09ff0f83aee1dc3e60
Dave <daveg7lee@gmail.com>
2022-02-05 20:31:18 +0900

    Home Screen part Two

--- src/Page/Home.tsx
+++ src/Page/Home.tsx
@@ -1,5 +1,59 @@
+import { useQuery } from 'react-query';
+import { getMovies, IGetMoviesResult } from '../api';
+import styled from 'styled-components';
+import { makeImagePath } from '../utils';
+
+const Wrapper = styled.div`
+  background-color: black;
+`;
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Banner = styled.div<{ bgPhoto: string }>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 3.75rem;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgPhoto});
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  font-size: 3.5rem;
+  margin-bottom: 10px;
+`;
+
+const Overview = styled.p`
+  font-size: 1.5rem;
+  width: 50%;
+`;
+
 function Home() {
-  return <div style={{ height: '300vh' }}></div>;
+  const { data, isLoading } = useQuery<IGetMoviesResult>(
+    ['movies', 'nowPlaying'],
+    getMovies
+  );
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading..</Loader>
+      ) : (
+        <>
+          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || '')}>
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+        </>
+      )}
+    </Wrapper>
+  );
 }
 
 export default Home;
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { GlobalStyle, theme } from './theme';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider } from 'styled-components';
+
+const client = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
