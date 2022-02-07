d6ff8ff747ba694b5431d6df6612f8e6b6391b67
Dave <daveg7lee@gmail.com>
2022-02-07 20:14:25 +0900

    use Basename

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Tv from './Page/Tv';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Header />
       <Routes>
         <Route path={'/'} element={<Home />} />
